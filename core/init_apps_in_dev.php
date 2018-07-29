e489d7ef408a70aecc972408178ba8c33b1d2263
Zsoner <zsoner@wsxhr.com>
2018-07-29 13:34:42 +0800

    修正生成文件

--- core/init_apps_in_dev.php
+++ core/init_apps_in_dev.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * 根据配置获取需要的启动文件
+ * @param $config
+ *
+ * @return array
+ * @Author  : linge@9rax.com
+ * @DateTime: 2018/7/29 13:27
+ */
 function get_bat_files($config){
 
     $type=implode('',$config[1]);
@@ -29,25 +37,33 @@ function get_bat_files($config){
 }
 
 
+/**
+ * 生成windows bat文件
+ * @param $config
+ * @param $name
+ *
+ * @Author  : linge@9rax.com
+ * @DateTime: 2018/7/29 13:28
+ */
 function build_bat_file($config,$name){
 
     global $argv;
 
-    $bat=dirname(__DIR__).DIRECTORY_SEPARATOR.$name.DIRECTORY_SEPARATOR.'start_win_'.$name.'.cmd';
+    $bat=dirname(__DIR__).DIRECTORY_SEPARATOR.$config[0].DIRECTORY_SEPARATOR.'start_win_'.$name.'.cmd';
 
     //if(!file_exists($bat) || strpos(implode('',$argv),'-f')!==false){
 
-        $files=glob(dirname(__DIR__).DIRECTORY_SEPARATOR.$name.DIRECTORY_SEPARATOR.'/client_service/start*.php');
+        $files=glob(dirname(__DIR__).DIRECTORY_SEPARATOR.$config[0].DIRECTORY_SEPARATOR.'/client_service/start*.php');
 
         $SERVER_PATH =dirname(__DIR__).DIRECTORY_SEPARATOR.$config[0].DIRECTORY_SEPARATOR.'client_service';
 
         $str ='';
 
+
         foreach ($files as $file){
             if(is_file($file)){
                 $str .='   '.$file;
             }
-
         }
 
         $command="";
@@ -66,9 +82,17 @@ function build_bat_file($config,$name){
 
         file_put_contents($bat,$command);
 
-    //}
 }
 
+
+/**
+ * 创建文件夹
+ * @param      $path
+ * @param bool $real_path
+ *
+ * @Author  : zaoyongvip@gmail.com
+ * @DateTime: 2018/7/29 13:18
+ */
 function _mkdir($path,$real_path=false){
     $path=$real_path?$path:dirname(__DIR__).DIRECTORY_SEPARATOR.$path;
     if(!is_dir($path)){
@@ -101,6 +125,13 @@ function copy_dir($src, $dst)
 }
 
 
+/**
+ * 初始化项目目录
+ * @param $config
+ *
+ * @Author  : linge@9rax.com
+ * @DateTime: 2018/7/29 13:26
+ */
 function init_app($config){
     //创建APP目录
     _mkdir($config[0]);
@@ -111,14 +142,14 @@ function init_app($config){
 
     copy_dir($from_dir,$to_dir);
 
-
     //删除不需要的原装启动文件
     $files=glob(dirname(__DIR__).'/'.$config[0].'/client_service/start*.php');
 
     $start_files=get_bat_files($config);
 
     foreach ($files as $file){
-        if(!in_array(basename($file),$start_files) && in_array(basename($file),['businessworker.php','gateway.php','register.php','web.php','queue.php',])){
+
+        if(!in_array(basename($file),$start_files) && in_array(basename($file),['start_businessworker.php','start_gateway.php','start_register.php','start_web.php','start_queue.php'])){
             unlink($file);
         }
     }
