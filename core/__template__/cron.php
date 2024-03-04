23daf5146bf721e5979cd2ac13a00647fee5c3ef
ForDever <9rax.dev@gmail.com>
2024-03-04 18:17:55 +0800

    模板文件

--- core/__template__/cron.php
+++ core/__template__/cron.php
@@ -2,7 +2,7 @@
 
 return [
     /*
-    ['name'=>'cron_1','type'=>'cron','time'=>'* * * * * *','callback'=>function(){ echo 'cron_1 :'.date('Y-m-d H:i:s')."\n";}],
+    ['name'=>'cron_1','type'=>'cron','time'=>'* * * * * *','callback'=>'app\__TEMPLATE__\service\Test:run'],
     ['name'=>'cron_2','type'=>'timer','time'=>1,'callback'=>function(){ echo 'cron_time :'.date('Y-m-d H:i:s')."\n";}]
     */
 ];
